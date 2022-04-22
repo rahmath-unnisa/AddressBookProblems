84c87d35519e65176efb1984b16eb96b61ba9a79
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-04-22 09:17:49 +0530

    [Rahmath]Add: Creating A New Contact

---
+++ AddressBookProblems/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace AddressBookProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Adress Book Program");
+            Contact contact = new Contact();
+            Console.WriteLine("Enter Your First Name : ");
+            contact.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Last Name : ");
+            contact.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Address : ");
+            contact.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter Your City : ");
+            contact.City = Console.ReadLine();
+
+            Console.WriteLine("Enter Your State : ");
+            contact.State = Console.ReadLine();
+
+            Console.WriteLine("Enter Your Zip Code : ");
+            contact.Zip = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Your Phone Number : ");
+            contact.PhoneNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Your Email : ");
+            contact.Email = Console.ReadLine();
+        }
+    }
+
+
+}
+   
